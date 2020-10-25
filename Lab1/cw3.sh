4fe0be0c455b629f23becf38843e1c78da9d676e
John15321 <johngadsden1775@gmail.com>
2020-10-25 15:22:58 +0100

    added tar compresion

--- Lab1/cw3.sh
+++ Lab1/cw3.sh
@@ -2,7 +2,7 @@
 
 # (3p.) Ćwiczenia z programem find: napisz skrypt znajdujący programem find wszystkie pliki o zadanym rozszerzeniu lub masce, które były modyfikowane w ciągu ostatnich N dni i tworzący z nich archiwum tar o zadanej nazwie. Mamy tu trzy parametry: maskę nazwy pliku, liczbę N i nazwę archiwum. Parametry są zadawane przez argumenty, to znaczy skrypt zawsze będzie wywołany z trzema parametrami.
 
-> "$3"
+mkdir "$3"
 for found_file in $(find . -name "*$1" -print);
 do
     # Gets the number of days since the last modification of the file
@@ -10,5 +10,7 @@ do
     now=$(date +%s)
     days=$(expr \( $now - $mod \) / 86400)
     # Check if the file modification date is in range
-    [ "$days" -le "$2" ] && echo "$found_file" >> "$3"
+    [ "$days" -le "$2" ] && cp $found_file "./$3"
 done
+tar -czvf "$3.tar.gz" "./$3"
+rm -rf "$3"
